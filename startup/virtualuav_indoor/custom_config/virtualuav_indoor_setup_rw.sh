a5cd34811fa5af10eb3e2f00a0a46e94f315580c
aivano <antun.ivanovic@fer.hr>
2023-09-26 16:22:02 +0200

    [Startup] Update config for virtualuav.

--- startup/virtualuav_indoor/custom_config/virtualuav_indoor_setup_rw.sh
+++ startup/virtualuav_indoor/custom_config/virtualuav_indoor_setup_rw.sh
@@ -31,25 +31,25 @@ if [[ -z "${ARENA_TYPE}" ]]
   fi;
 
 # Global Planner parameters
-export ABSOLUTE_CONFIG=true
-export MAP_CONFIG=$(rospack find icuas23_competition)/config/global_planner.yaml
-export TRAJ_CONFIG=$(rospack find icuas23_competition)/config/global_planner.yaml
-export STATE_VALIDITY_CONFIG=$(rospack find icuas23_competition)/config/global_planner.yaml
-export PATH_PLANNER_CONFIG=$(rospack find icuas23_competition)/config/global_planner.yaml
-export MODEL_CORRECTION_CONFIG=$(rospack find larics_motion_planning)/config/model_correction_config_example.yaml
+#export ABSOLUTE_CONFIG=true
+#export MAP_CONFIG=$(rospack find icuas23_competition)/config/global_planner.yaml
+#export TRAJ_CONFIG=$(rospack find icuas23_competition)/config/global_planner.yaml
+#export STATE_VALIDITY_CONFIG=$(rospack find icuas23_competition)/config/global_planner.yaml
+#export PATH_PLANNER_CONFIG=$(rospack find icuas23_competition)/config/global_planner.yaml
+#export MODEL_CORRECTION_CONFIG=$(rospack find larics_motion_planning)/config/model_correction_config_example.yaml
 
 # Empty map
 # export OCTOMAP_FILE=$(rospack find larics_motion_planning)/config/empty_map.binvox.bt
 
 # Uncomment this if using a real map
 # export OCTOMAP_FILE=$(pwd)/custom_config/icuas2022_arena_latest.binvox.bt
-export OCTOMAP_FILE=$(rospack find icuas_support)/resources/arena_warszawa_2.binvox.bt
+#export OCTOMAP_FILE=$(rospack find icuas_support)/resources/arena_warszawa_2.binvox.bt
 
 # Optitrack parameters
 export OBJECT_NAME=falconblack
 export ODOM_TOPIC=/$OBJECT_NAME/vrpn_client/estimated_odometry
 export OPTITRACK_IP=192.168.1.50
-export GEOFENCE_CONFIG=$(pwd)/custom_config/geofence_config.yaml
+#export GEOFENCE_CONFIG=$(pwd)/custom_config/geofence_config.yaml
 
 # Pixhawk
 export UAV_NAMESPACE=red
