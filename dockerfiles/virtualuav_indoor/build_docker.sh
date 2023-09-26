7319a27b196ba0d743653196f6f2bd17bda6a153
hunterls97 <hunterls@my.yorku.ca>
2023-09-26 18:05:19 +0200

    [Docker] Update docker config.

--- dockerfiles/virtualuav_indoor/build_docker.sh
+++ dockerfiles/virtualuav_indoor/build_docker.sh
@@ -6,4 +6,9 @@ export DOCKER_BUILDKIT=1
 # Add ssh key and enter passphrase.
 # This can be avoided if a key without passphrase is used.
 #ssh-add ~/.ssh/id_ed25519
-docker build --ssh default -t virtualuav_indoor_img .
+docker build \
+  --ssh default \
+  --build-arg ROS_HOSTNAME=$ROS_HOSTNAME \
+  --build-arg ROS_MASTER_URI=$ROS_MASTER_URI \
+  --build-arg ROS_IP=$ROS_IP \
+  -t virtualuav_indoor_img .
