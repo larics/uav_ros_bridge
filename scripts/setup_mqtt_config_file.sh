a999095c99cebdbc97643b3450f845ace63c8c6a
aivano <antun.ivanovic@fer.hr>
2023-09-27 12:36:16 +0200

    [Scripts] Add option to change IP for mqtt config.

--- scripts/setup_mqtt_config_file.sh
+++ scripts/setup_mqtt_config_file.sh
@@ -4,9 +4,20 @@
 # is that it does not support environment variables. That is why the config
 # file is copied to .config and multiple parameters are changed according to
 # environment variables.
-cp $1 $(rospack find uav_ros_bridge)/config/.config.yaml
-sed -i "s/uav_unique_namespace/${UAV_NAMESPACE}/" $(rospack find uav_ros_bridge)/config/.config.yaml
+CFG_FILE=$(rospack find uav_ros_bridge)/config/.config.yaml
+cp $1 $CFG_FILE
+sed -i "s/uav_unique_namespace/${UAV_NAMESPACE}/" $CFG_FILE
 
 # Load rosparam here. This will ensure the parameter is loaded before running
 # the mqtt communication node.
-rosparam load $(rospack find uav_ros_bridge)/config/.config.yaml $UAV_NAMESPACE/mqtt_bridge
+rosparam load $CFG_FILE $UAV_NAMESPACE/mqtt_bridge
+
+# Change ip address for the mqtt server if available.
+if [ -z "$UAV_IP" ]
+then
+  #echo "\$UAV_IP is empty. Defaulting to localhost."
+  sed -i "s/host: uav_unique_ip/host: localhost/" $CFG_FILE
+else
+  #echo "\$UAV_IP is NOT empty. IP will be set to $UAV_IP."
+  sed -i "s/host: uav_unique_ip/host: $UAV_IP/" $CFG_FILE
+fi
