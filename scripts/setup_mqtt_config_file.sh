38574b2c615290089da92086ef14a88a04310ff5
aivano <antun.ivanovic@fer.hr>
2023-09-27 13:36:11 +0200

    [Scripts] Reorganize script.

--- scripts/setup_mqtt_config_file.sh
+++ scripts/setup_mqtt_config_file.sh
@@ -8,10 +8,6 @@ CFG_FILE=$(rospack find uav_ros_bridge)/config/.config.yaml
 cp $1 $CFG_FILE
 sed -i "s/uav_unique_namespace/${UAV_NAMESPACE}/" $CFG_FILE
 
-# Load rosparam here. This will ensure the parameter is loaded before running
-# the mqtt communication node.
-rosparam load $CFG_FILE $UAV_NAMESPACE/mqtt_bridge
-
 # Change ip address for the mqtt server if available.
 if [ -z "$UAV_IP" ]
 then
@@ -19,5 +15,9 @@ then
   sed -i "s/host: uav_unique_ip/host: localhost/" $CFG_FILE
 else
   #echo "\$UAV_IP is NOT empty. IP will be set to $UAV_IP."
-  sed -i "s/host: uav_unique_ip/host: $UAV_IP/" $CFG_FILE
+  sed -i "s/host: uav_unique_ip/host: ${UAV_IP}/" $CFG_FILE
 fi
+# Load rosparam here. This will ensure the parameter is loaded before running
+# the mqtt communication node.
+rosparam load $CFG_FILE $UAV_NAMESPACE/mqtt_bridge
+
