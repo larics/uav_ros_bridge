b6d91accc3930b0cb5cda4f234b327ddb81fe2c6
aivano <antun.ivanovic@fer.hr>
2024-09-09 18:21:58 +0200

    [Scripts] Add example for InspectionTrajectoryGui from pcl_ris_msgs.

--- scripts/ros2json_composite.py
+++ scripts/ros2json_composite.py
@@ -5,11 +5,46 @@ import sys
 import importlib
 import copy
 from uav_ros_msgs.msg import Waypoint, Waypoints, Task
+from pcl_ris_msgs.msg import InspectionTrajectoryGui
+from geometry_msgs.msg import Pose
+from std_msgs.msg import Float64MultiArray, MultiArrayDimension
 
 def msg2json(msg):
     ''' Convert a ROS message to JSON format'''
     y = yaml.load(str(msg))
     return json.dumps(y,indent=2)
+
+def createInspectionTrajectoryGuiExample(n):
+    traj = InspectionTrajectoryGui()
+    for i in range(n):
+        pose = Pose()
+        pose.position.x = i
+        traj.poses.append(pose)
+    
+    matrix = [
+        [1.0, 2.0, 3.0, 4.0],
+        [5.0, 6.0, 7.0, 8.0],
+        [9.0, 10.0, 11.0, 12.0],
+        [13.0, 14.0, 15.0, 16.0]
+    ]
+
+    # Set up dimensions for a 4x4 matrix
+    dim_row = MultiArrayDimension()
+    dim_row.label = "rows"
+    dim_row.size = 4
+    dim_row.stride = 4 * 4  # Total number of elements (4x4)
+
+    dim_col = MultiArrayDimension()
+    dim_col.label = "columns"
+    dim_col.size = 4
+    dim_col.stride = 4
+
+    traj.transform.layout.dim = [dim_row, dim_col]
+
+    # Flatten the matrix and assign the data
+    traj.transform.data = [elem for row in matrix for elem in row]  # Flattening the matrix row-wise
+
+    return traj
 
 if __name__ == "__main__":
     task = Task()
@@ -37,3 +72,7 @@ if __name__ == "__main__":
 
     print("Waypoints message")
     print(msg2json(waypoints))
+
+    # InspectionTrajectory
+    print("Inspection trajectory GUI message")
+    print(msg2json(createInspectionTrajectoryGuiExample(2)))
